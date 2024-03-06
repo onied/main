58e9867fb580f1f2765282abbe97c3083017f9a4
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-06 17:19:17 +0300

    MAIN-11: Added stub for hierarchy

--- frontend/onied/src/components/sidebar/sidebar.jsx
+++ frontend/onied/src/components/sidebar/sidebar.jsx
@@ -1,26 +1,57 @@
+import { useState } from "react";
 import { CompletedIcon } from "./completedIcon";
 import classes from "./sidebar.module.css";
 
 function Sidebar() {
+  const [hierarchy, setHierarchy] = useState({
+    title: "нАзвАниЕ курсА бЕз рЕгистрА",
+    modules: [
+      {
+        title: "Первый модуль",
+        blocks: [
+          { title: "Первый блок", completed: false },
+          { title: "Второй блок", completed: false },
+          { title: "Третий блок", completed: true },
+          { title: "Четвертый блок", completed: false },
+        ],
+      },
+      {
+        title: "Второй модуль",
+        blocks: [
+          { title: "Первый блок", completed: false },
+          { title: "Второй блок", completed: false },
+          { title: "Третий блок", completed: false },
+          { title: "Четвертый блок", completed: false },
+        ],
+      },
+    ],
+  });
+  const renderBlock = (block, index, moduleIndex) => {
+    return (
+      <div className={classes.block}>
+        {moduleIndex + 1}.{index + 1}. {block.title}
+        {block.completed ? <CompletedIcon></CompletedIcon> : <></>}
+      </div>
+    );
+  };
+  const renderModule = (module, index) => {
+    return (
+      <>
+        <div className={classes.module}>
+          {index + 1}. {module.title}
+        </div>
+        <div>
+          {module.blocks.map((block, blockIndex) =>
+            renderBlock(block, blockIndex, index)
+          )}
+        </div>
+      </>
+    );
+  };
   return (
     <div className={classes.sidebar}>
-      <div className={classes.title}>название курса без регистра</div>
-      <div>
-        <div className={classes.module}>1. Первый модуль</div>
-        <div className={classes.block}>1.1. Первый блок</div>
-        <div className={classes.selected}>1.2. Второй блок</div>
-        <div className={classes.block}>
-          1.3. Третий блок <CompletedIcon></CompletedIcon>
-        </div>
-        <div className={classes.block}>1.4. Четвертый блок</div>
-      </div>
-      <div>
-        <div className={classes.module}>2. Второй модуль</div>
-        <div className={classes.block}>2.1. Первый блок</div>
-        <div className={classes.block}>2.2. Второй блок</div>
-        <div className={classes.block}>2.3. Третий блок</div>
-        <div className={classes.block}>2.4. Четвертый блок</div>
-      </div>
+      <div className={classes.title}>{hierarchy.title.toLowerCase()}</div>
+      <div>{hierarchy.modules.map(renderModule)}</div>
     </div>
   );
 }
