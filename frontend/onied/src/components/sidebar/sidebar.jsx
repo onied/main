52e38b5f11170efce1fae687242b887d36f1eab1
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-06 15:52:39 +0300

    Added title to sidebar

--- frontend/onied/src/components/sidebar/sidebar.jsx
+++ frontend/onied/src/components/sidebar/sidebar.jsx
@@ -1,7 +1,11 @@
 import classes from "./sidebar.module.css";
 
 function Sidebar() {
-  return <div className={classes.sidebar}></div>;
+  return (
+    <div className={classes.sidebar}>
+      <div className={classes.title}>название курса без регистра</div>
+    </div>
+  );
 }
 
 export default Sidebar;
