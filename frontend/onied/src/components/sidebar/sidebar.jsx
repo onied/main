08d69c31b02a63c0da4cc31d64dcf027646a6b76
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-06 16:18:13 +0300

    MAIN-11: Added blocks and modules

--- frontend/onied/src/components/sidebar/sidebar.jsx
+++ frontend/onied/src/components/sidebar/sidebar.jsx
@@ -4,6 +4,20 @@ function Sidebar() {
   return (
     <div className={classes.sidebar}>
       <div className={classes.title}>название курса без регистра</div>
+      <div>
+        <div className={classes.module}>1. Первый модуль</div>
+        <div className={classes.block}>1.1. Первый блок</div>
+        <div className={classes.block}>1.2. Второй блок</div>
+        <div className={classes.block}>1.3. Третий блок</div>
+        <div className={classes.block}>1.4. Четвертый блок</div>
+      </div>
+      <div>
+        <div className={classes.module}>2. Второй модуль</div>
+        <div className={classes.block}>2.1. Первый блок</div>
+        <div className={classes.block}>2.2. Второй блок</div>
+        <div className={classes.block}>2.3. Третий блок</div>
+        <div className={classes.block}>2.4. Четвертый блок</div>
+      </div>
     </div>
   );
 }
