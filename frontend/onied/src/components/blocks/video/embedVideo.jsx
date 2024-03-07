8395677ab37f9304152a75653369c484a941de0b
Matvey <enhisir@localhost.localdomain>
2024-03-07 23:19:55 +0300

    moved classes to other files, fixed inheritance

--- frontend/onied/src/components/blocks/video/embedVideo.jsx
+++ frontend/onied/src/components/blocks/video/embedVideo.jsx
@@ -1,69 +1,10 @@
 import { useState } from "react";
 import classes from "./embedVideo.module.css"
+import YoutubeVideoProvider from "./youtubeVideoProvider"
+import VkVideoProvider from "./vkVideoProvider"
+import RutubeVideoProvider from "./rutubeVideoProvider"
 
-/**
- * Abstract Class VideoProvider.
- *
- * @class VideoProvider
- */
-class VideoProvider
-{   
-    constructor() {
-        if (this.constructor == VideoProvider) {
-            throw new Error("Abstract classes can't be instantiated.");
-        }
-    }
 
-    // regex: регулярное выражение, соответствующее видеопровайдеру.
-    regex = new RegExp();
-    
-    // getIFrameLink: функция, которая конвертирует ссылку в ссылку для iframe.
-    // regex должен содержать именованные группы, содержащиеся в convertHrefToIFrameSrc.
-    getLink(href) {
-        throw new Error("Method 'getLink()' must be implemented.");
-    }
-}
-
-class YoutubeVideoProvider
-{   
-    constructor() {}
-
-    regex = /^((?:https?:)?\/\/)?((?:www|m).)?((?:youtube(-nocookie)?.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|live\/|v\/)?)(?<videoId>[\w]+)(\S+)?$/;
-    
-    getLink(href) {
-        const matches = href.match(this.regex);
-
-        return `https://www.youtube.com/embed/${matches.groups.videoId}`;
-    }
-}
-
-class VkVideoProvider
-{   
-    constructor() {}
-
-    regex = /^(?:https:\/\/)?(?:www\.)?(?:vk\.com|m\.vk\.com)(?:\/video)(?<videoOid>[\d-]+)_(?<videoId>[\d]+)$/;
-    
-    getLink(href) {
-        const matches = href.match(this.regex);
-
-        return `https://vk.com/video_ext.php?oid=${matches.groups.videoOid}&id=${matches.groups.videoId}`;
-    }
-}
-
-class RutubeVideoProvider
-{   
-    constructor() {}
-
-    regex = /^((?:https?:)?\/\/)?(rutube\.ru)(\/video)(?<videoId>\/[\w\d]+)?(\/[\S]+)?$/;
-    
-    getLink(href) {
-        const matches = href.match(this.regex);
-        
-        return `https://rutube.ru/play/embed/${matches.groups.videoId}`;
-    }
-}
-
-// 
 const embedElements = [
     new YoutubeVideoProvider(),
     new VkVideoProvider(),
