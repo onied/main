fcd275dc809bd5b9fcc2af357e4ec20393009518
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-07 18:53:41 +0300

    Fixed scalability

--- frontend/onied/src/components/blocks/tasks/generalTask.jsx
+++ frontend/onied/src/components/blocks/tasks/generalTask.jsx
@@ -7,22 +7,16 @@ import ManualReviewTask from "./manualReviewTask";
 import classes from "./tasks.module.css";
 
 function GeneralTask({ task, index }) {
+  const bodies = [
+    SingleAnswersTask,
+    MultipleAnswersTask,
+    InputAnswer,
+    ManualReviewTask,
+  ];
   return (
     <div>
       <TaskTitle taskTitle={task.title} pointInfo={task.pointInfo}></TaskTitle>
-      <div className={classes.taskBody}>
-        {task.type == taskType.SINGLE_ANSWER ? (
-          <SingleAnswersTask task={task}></SingleAnswersTask>
-        ) : task.type == taskType.MULTIPLE_ANSWERS ? (
-          <MultipleAnswersTask task={task}></MultipleAnswersTask>
-        ) : task.type == taskType.INPUT_ANSWER ? (
-          <InputAnswer task={task}></InputAnswer>
-        ) : task.type == taskType.REVIEW_ANSWER ? (
-          <ManualReviewTask task={task}></ManualReviewTask>
-        ) : (
-          <></>
-        )}
-      </div>
+      <div className={classes.taskBody}>{bodies[task.type]({ task })}</div>
     </div>
   );
 }
