da78882bc95d4f60a6847b62feef0b1b684b0289
Matvey <enhisir@localhost.localdomain>
2024-03-07 23:09:11 +0300

    moved providers into classes

--- frontend/onied/src/components/blocks/video/embedVideo.jsx
+++ frontend/onied/src/components/blocks/video/embedVideo.jsx
@@ -1,41 +1,76 @@
 import { useState } from "react";
 import classes from "./embedVideo.module.css"
 
-// 
-const embedElements = [
-    {
-        // youtube
-        regex: /^((?:https?:)?\/\/)?((?:www|m).)?((?:youtube(-nocookie)?.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|live\/|v\/)?)(?<videoId>[\w-]+)(\S+)?$/,
-        getLink: (href) => {
-            const regex = /^((?:https?:)?\/\/)?((?:www|m).)?((?:youtube(-nocookie)?.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|live\/|v\/)?)(?<videoId>[\w]+)(\S+)?$/;
-            const matches = href.match(regex);
-
-            return `https://www.youtube.com/embed/${matches.groups.videoId}`;
-        }
-    },
-    {
-        // vk video
-        regex: /^(?:https:\/\/)?(?:www\.)?(?:vk\.com|m\.vk\.com)(?:\/video)(?<videoOid>[\d-]+)_(?<videoId>[\d]+)$/,
-        getLink: (href) => {
-            const regex = /^(?:https:\/\/)?(?:www\.)?(?:vk\.com|m\.vk\.com)(?:\/video)(?<videoOid>[\d-]+)_(?<videoId>[\d]+)$/;
-            const matches = href.match(regex);
-            
-            return `https://vk.com/video_ext.php?oid=${matches.groups.videoOid}&id=${matches.groups.videoId}`;
-        }
-    },
-    {
-        // rutube
-        regex: /^((?:https?:)?\/\/)?(rutube\.ru)(\/video)(?<videoId>\/[\w\d]+)?(\/[\S]+)?$$/,
-        getLink: (href) => {
-            const regex = /^((?:https?:)?\/\/)?(rutube\.ru)(\/video)(?<videoId>\/[\w\d]+)(\/[\S]+)?$/;
-            const matches = href.match(regex);
-            
-            return `https://rutube.ru/play/embed/${matches.groups.videoId}`;
+/**
+ * Abstract Class VideoProvider.
+ *
+ * @class VideoProvider
+ */
+class VideoProvider
+{   
+    constructor() {
+        if (this.constructor == VideoProvider) {
+            throw new Error("Abstract classes can't be instantiated.");
         }
     }
+
+    // regex: регулярное выражение, соответствующее видеопровайдеру.
+    regex = new RegExp();
+    
+    // getIFrameLink: функция, которая конвертирует ссылку в ссылку для iframe.
+    // regex должен содержать именованные группы, содержащиеся в convertHrefToIFrameSrc.
+    getLink(href) {
+        throw new Error("Method 'getLink()' must be implemented.");
+    }
+}
+
+class YoutubeVideoProvider
+{   
+    constructor() {}
+
+    regex = /^((?:https?:)?\/\/)?((?:www|m).)?((?:youtube(-nocookie)?.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|live\/|v\/)?)(?<videoId>[\w]+)(\S+)?$/;
+    
+    getLink(href) {
+        const matches = href.match(this.regex);
+
+        return `https://www.youtube.com/embed/${matches.groups.videoId}`;
+    }
+}
+
+class VkVideoProvider
+{   
+    constructor() {}
+
+    regex = /^(?:https:\/\/)?(?:www\.)?(?:vk\.com|m\.vk\.com)(?:\/video)(?<videoOid>[\d-]+)_(?<videoId>[\d]+)$/;
+    
+    getLink(href) {
+        const matches = href.match(this.regex);
+
+        return `https://vk.com/video_ext.php?oid=${matches.groups.videoOid}&id=${matches.groups.videoId}`;
+    }
+}
+
+class RutubeVideoProvider
+{   
+    constructor() {}
+
+    regex = /^((?:https?:)?\/\/)?(rutube\.ru)(\/video)(?<videoId>\/[\w\d]+)?(\/[\S]+)?$/;
+    
+    getLink(href) {
+        const matches = href.match(this.regex);
+        
+        return `https://rutube.ru/play/embed/${matches.groups.videoId}`;
+    }
+}
+
+// 
+const embedElements = [
+    new YoutubeVideoProvider(),
+    new VkVideoProvider(),
+    new RutubeVideoProvider(),
 ];
 
-function videoLinkToVideoProvider(href) {
+function videoLinkToIFrame(href) {
     const embedRegex = embedElements.filter((item) => item.regex.test(href));
 
     const iframeLink = embedRegex[0].getLink(href); 
@@ -51,7 +86,7 @@ function videoLinkToVideoProvider(href) {
 
 function EmbedVideo({ href }) {
 
-    const [videoProvider, setVideoProvider] = useState(videoLinkToVideoProvider(href));
+    const [videoProvider, setVideoProvider] = useState(videoLinkToIFrame(href));
 
     return <div className={classes.embedVideo}>
         {videoProvider}
