cac222d5f86b96d1c7722f79a33d97f5192a6517
Matvey <enhisir@localhost.localdomain>
2024-03-07 22:41:59 +0300

    moved iframe logic to another function

--- frontend/onied/src/components/blocks/video/embedVideo.jsx
+++ frontend/onied/src/components/blocks/video/embedVideo.jsx
@@ -6,62 +6,52 @@ const embedElements = [
     {
         // youtube
         regex: /^((?:https?:)?\/\/)?((?:www|m).)?((?:youtube(-nocookie)?.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|live\/|v\/)?)(?<videoId>[\w-]+)(\S+)?$/,
-        getProvider: (href) => {
+        getLink: (href) => {
             const regex = /^((?:https?:)?\/\/)?((?:www|m).)?((?:youtube(-nocookie)?.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|live\/|v\/)?)(?<videoId>[\w]+)(\S+)?$/;
             const matches = href.match(regex);
 
-            return <iframe 
-            src={`https://www.youtube.com/embed/${matches.groups.videoId}`}
-            className={classes.embedIFrame}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            allowFullScreen />;
+            return `https://www.youtube.com/embed/${matches.groups.videoId}`;
         }
     },
     {
         // vk video
         regex: /^(?:https:\/\/)?(?:www\.)?(?:vk\.com|m\.vk\.com)(?:\/video)(?<videoOid>[\d-]+)_(?<videoId>[\d]+)$/,
-        getProvider: (href) => {
+        getLink: (href) => {
             const regex = /^(?:https:\/\/)?(?:www\.)?(?:vk\.com|m\.vk\.com)(?:\/video)(?<videoOid>[\d-]+)_(?<videoId>[\d]+)$/;
             const matches = href.match(regex);
             
-            return <iframe 
-            src={`https://vk.com/video_ext.php?oid=${matches.groups.videoOid}&id=${matches.groups.videoId}`}
-            className={classes.embedIFrame}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"           
-            allowFullScreen />;
+            return `https://vk.com/video_ext.php?oid=${matches.groups.videoOid}&id=${matches.groups.videoId}`;
         }
     },
     {
         // rutube
         regex: /^((?:https?:)?\/\/)?(rutube\.ru)(\/video)(?<videoId>\/[\w\d]+)?(\/[\S]+)?$$/,
-        getProvider: (href) => {
+        getLink: (href) => {
             const regex = /^((?:https?:)?\/\/)?(rutube\.ru)(\/video)(?<videoId>\/[\w\d]+)(\/[\S]+)?$/;
             const matches = href.match(regex);
-            // https://rutube.ru/video/1c69be7b3e28cb58368f69473f6c1d96/?r=wd
             
-            
-            return <iframe 
-            src={`https://rutube.ru/play/embed/${matches.groups.videoId}`} 
-            className={classes.embedIFrame}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen />;
+            return `https://rutube.ru/play/embed/${matches.groups.videoId}`;
         }
     }
 ];
 
-function videoLinkToEmbedLink(href) {
+function videoLinkToVideoProvider(href) {
     const embedRegex = embedElements.filter((item) => item.regex.test(href));
 
+    const iframeLink = embedRegex[0].getLink(href); 
     if (embedRegex.length == 0)
         throw Error("Неверный формат ссылки на видео");
 
-    return embedRegex[0].getProvider(href);
+    return <iframe 
+    src={iframeLink} 
+    className={classes.embedIFrame}
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowFullScreen />;
 }
 
 function EmbedVideo({ href }) {
 
-    const [videoProvider, setVideoProvider] = useState(videoLinkToEmbedLink(href));
+    const [videoProvider, setVideoProvider] = useState(videoLinkToVideoProvider(href));
 
     return <div className={classes.embedVideo}>
         {videoProvider}
