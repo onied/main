80ad59a6001fba76d512982dd117cba78a14e464
Matvey <enhisir@localhost.localdomain>
2024-03-08 09:52:23 +0300

    fixed blocks.module.css import

--- frontend/onied/src/components/blocks/blockViewContainer.jsx
+++ frontend/onied/src/components/blocks/blockViewContainer.jsx
@@ -1,4 +1,4 @@
-import classes from "../../components/blocks/blocks.module.css"
+import classes from "./blocks.module.css"
 
 export default function BlockViewContainer({ children }) {
     return (<div className={classes.blockViewContainer}>{ children }</div>);
