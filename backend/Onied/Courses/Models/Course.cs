6171d5eb15b6a5bb0d0b805e71305529af408e15
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-10 20:36:48 +0300

    Updated course model

--- backend/Onied/Courses/Models/Course.cs
+++ backend/Onied/Courses/Models/Course.cs
@@ -6,9 +6,35 @@ public class Course
 {
     public int Id { get; set; }
 
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
     [MinLength(1)]
     [MaxLength(200)]
     public string Title { get; set; } = null!;
+
+    [Url]
+    [MaxLength(2048)]
+    public string PictureHref { get; set; } = null!;
+
+    [MinLength(1)]
+    [MaxLength(15000)]
+    public string Description { get; set; } = null!;
+
+    [Range(0, 35000)]
+    public int HoursCount { get; set; }
+
+    [Range(0, 1000000)]
+    public int PriceRubles { get; set; }
+
+    public bool IsArchived { get; set; }
+
+    public bool IsGlowing { get; set; }
+
+    public bool HasCertificates { get; set; }
 
     public ICollection<Module> Modules { get; } = new List<Module>();
 }
