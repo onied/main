a6dda1ebdb2753640729b73b459a6f0c140a4d11
Emin <emin.sarux2004@yandex.ru>
2024-03-11 20:37:58 +0300

    added page navigation

--- frontend/onied/src/components/catalog/courseCardsContainer.jsx
+++ frontend/onied/src/components/catalog/courseCardsContainer.jsx
@@ -1,37 +1,14 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import GeneralCourseCard from "./courseCards/generalCourseCard.jsx";
 import classes from "./courseCardsContainer.module.css"
+import {getPage} from "./catalogPages.js";
 
-function CourseCardsContainer(){
-    const plainCourse = {
-        href: "https://intelligentemployment.com/wp-content/uploads/2021/12/AdobeStock_387227165-scaled.jpeg",
-        courseTitle: "Создание голограмм на ноутбуке",
-        category: "Категория",
-        courseAuthor: "Автор курса",
-        coursePrice: 7777,
-        isHighlighted: false
-    };
-    const freeCourse = {
-        href: "https://intelligentemployment.com/wp-content/uploads/2021/12/AdobeStock_387227165-scaled.jpeg",
-        courseTitle: "Создание голограмм на ноутбуке(демо)",
-        category: "Категория",
-        courseAuthor: "Автор курса",
-        coursePrice: 0,
-        isHighlighted: false
-    };
-    const highlightedCourse = {
-        href: "https://intelligentemployment.com/wp-content/uploads/2021/12/AdobeStock_387227165-scaled.jpeg",
-        courseTitle: "Создание голограмм на ноутбуке(deluxe edition)",
-        category: "Категория",
-        courseAuthor: "Автор курса",
-        coursePrice: 77777,
-        isHighlighted: true
-    };
-    const [coursesList, setCoursesList] = useState([
-        ...Array.from({ length: 5 }, () => ({ ...highlightedCourse })),
-        ...Array.from({ length: 10 }, () => ({ ...freeCourse })),
-        ...Array.from({ length: 5 }, () => ({ ...plainCourse })),
-    ]);
+function CourseCardsContainer({ currentPage }){
+    const [coursesList, setCoursesList] = useState([]);
+
+    useEffect(() => {
+        setCoursesList(getPage(currentPage));
+    }, [currentPage]);
 
     return(
         <div className={classes.courseCardsContainer}>
--- frontend/onied/src/pages/catalog/catalog.jsx
+++ frontend/onied/src/pages/catalog/catalog.jsx
@@ -1,9 +1,33 @@
 import CourseCardsContainer from "../../components/catalog/courseCardsContainer.jsx";
+import CatalogHeader from "../../components/catalog/catalogHeader/catalogHeader.jsx";
+import CatalogNavigation from "../../components/catalog/catalogNavigation/catalogNavigation.jsx";
+import {useEffect, useState} from "react";
+import {getAmountOfPages} from "../../components/catalog/catalogPages.js";
 
 function Catalog(){
+    const [currentPage, setCurrentPage] = useState(1);
+
+    function onPageChange(pageNumber){
+        if (pageNumber < 1){
+            setCurrentPage(1);
+        }
+        else if (pageNumber > getAmountOfPages()){
+            setCurrentPage(getAmountOfPages());
+        }
+        else{
+            setCurrentPage(pageNumber);
+        }
+    }
+
+    useEffect(() => {
+        console.log(currentPage);
+    }, [currentPage]);
+
     return(
         <div>
-            <CourseCardsContainer />
+            <CatalogHeader />
+            <CourseCardsContainer currentPage={currentPage}/>
+            <CatalogNavigation currentPage={currentPage} onPageChange={onPageChange}/>
         </div>
     )
 }
