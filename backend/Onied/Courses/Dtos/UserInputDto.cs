cedd6b0b4bd47fcb11f78a1f4d07178381394b71
Matvey <enhisir@localhost.localdomain>
2024-03-13 17:29:26 +0300

    fixed bug with validation

--- backend/Onied/Courses/Dtos/UserInputDto.cs
+++ backend/Onied/Courses/Dtos/UserInputDto.cs
@@ -19,22 +19,22 @@ public class UserInputDto : IValidatableObject
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
         if (TaskType is TaskType.SingleAnswer 
-            && !(Text is not null && Answer is not null))
+            && !(Text is null && Answer is null))
             yield return new ValidationResult(
                 "Task with SingleAnswer should contain only VariantIds");
         
         if (TaskType is TaskType.MultipleAnswers 
-            && !(Text is not null && Answer is not null))
+            && !(Text is null && Answer is null))
             yield return new ValidationResult(
                 "Task with MultipleAnswers should only VariantIds");
         
         if (TaskType is TaskType.InputAnswer
-            && !(VariantsIds is not null && Text is not null))
+            && !(VariantsIds is null && Text is null))
             yield return new ValidationResult(
                 "Task with InputAnswer should contain only Answer");
         
         if (TaskType is TaskType.ManualReview 
-            && !(VariantsIds is not null && Answer is not null))
+            && !(VariantsIds is null && Answer is null))
             yield return new ValidationResult(
                 "Task with ManualReview should contain only Text");
     }
