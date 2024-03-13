9797e0cb5eccffd83fa819b37f5d1b54924d8d74
Matvey <enhisir@localhost.localdomain>
2024-03-13 16:30:09 +0300

    removed useless commentary

--- backend/Onied/Courses/Program.cs
+++ backend/Onied/Courses/Program.cs
@@ -11,9 +11,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-/*builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
-    optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("CoursesDatabase"))
-        .UseSnakeCaseNamingConvention());*/
 builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
     optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("CoursesDatabase"))
         .UseSnakeCaseNamingConvention());
