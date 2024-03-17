c11e27b7654508ff58de20148572e49641a43953
antonborisov571 <antonborisov571@gmail.com>
2024-03-18 00:51:07 +0300

    Added page registration

---
+++ frontend/onied/src/pages/register/register.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/general/button/button";
+import InputForm from "../../components/general/inputform/inputform.jsx";
+import Radio from "../../components/general/radio/radio";
+import RegistrationForm from "../../components/register/registrationForm/registrationForm.jsx";
+import classes from "./register.module.css";
+
+function Register() {
+  return <RegistrationForm></RegistrationForm>;
+}
+
+export default Register;
