231c37f40f06b92caf5ecb34ff55e01ee2de4675
Matvey <enhisir@localhost.localdomain>
2024-03-23 19:39:08 +0300

    moved six digits to separate component

--- frontend/onied/src/components/accountManagement/twoFactorAuth/cardTwoFactor/cardTwoFactor.jsx
+++ frontend/onied/src/components/accountManagement/twoFactorAuth/cardTwoFactor/cardTwoFactor.jsx
@@ -1,20 +1,18 @@
 import classes from "./cardTwoFactor.module.css";
 import Card from "../../../general/card/card.jsx";
 import TwoFactorImg from "../../../../assets/twoFactor.svg";
-import SingleDigitInput from "../singleDigitInput/singleDigitInput.jsx";
 import Button from "../../../general/button/button.jsx";
-import { useRef, useState, useEffect } from "react";
-import axios, { Axios, AxiosError } from "axios";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import config from "../../../../config/config.js";
-import { handleErrors } from "../../register/registrationForm/validation.js";
 import { useNavigate, useLocation } from "react-router-dom";
+import SixDigitsInput from "../../sixDigitsInput/sixDigitsInput";
 
 function CardTwoFactor() {
   const navigator = useNavigate();
   const location = useLocation();
-  const inputsRefs = useRef([]);
 
-  const [digits, setDigits] = useState(["", "", "", "", "", ""]);
+  const [digits, setDigits] = useState("");
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [errorMessage, setErrorMessage] = useState("");
@@ -32,46 +30,13 @@ function CardTwoFactor() {
     }
   }, []);
 
-  const handleChange = (index, event) => {
-    const value = event.target.value;
-    if (value === "") {
-      handleBackspace(index);
-    } else if (/^\d$/.test(value)) {
-      handleInput(index, value);
-    }
-  };
-
-  const handleInput = (index, value) => {
-    setDigits((prevDigits) => {
-      const newDigits = [...prevDigits];
-      newDigits[index] = value;
-      return newDigits;
-    });
-    const nextIndex = index + 1;
-    if (nextIndex < inputsRefs.current.length) {
-      inputsRefs.current[nextIndex].focus();
-    }
-  };
-
-  const handleBackspace = (index) => {
-    setDigits((prevDigits) => {
-      const newDigits = [...prevDigits];
-      newDigits[index] = "";
-      return newDigits;
-    });
-    const prevIndex = index - 1;
-    if (prevIndex >= 0) {
-      inputsRefs.current[prevIndex].focus();
-    }
-  };
-
   const sendCode = () => {
-    if (digits.every((digit) => digit !== "")) {
+    if (digits !== "") {
       axios
         .post(config.UsersBackend + "login", {
           email: email,
           password: password,
-          twoFactorCode: digits.join(""),
+          twoFactorCode: digits,
         })
         .then((response) => {
           console.log(response);
@@ -113,18 +78,7 @@ function CardTwoFactor() {
             Введите код, сгенерированный вашим приложением <br />
             для 2-факторной аутентификации
           </div>
-          <div className={classes.inputDigits}>
-            {[...Array(6).keys()].map((index) => (
-              <SingleDigitInput
-                key={index}
-                id={`input-${index}`}
-                innerRef={(input) => (inputsRefs.current[index] = input)}
-                onChange={(event) => handleChange(index, event)}
-                className={classes.singleDigitInput}
-                value={digits[index]}
-              />
-            ))}
-          </div>
+          <SixDigitsInput onChange={setDigits} />
           {errorMessage && (
             <span className={classes.errorMessage}>{errorMessage}</span>
           )}
