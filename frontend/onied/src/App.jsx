48fdbf7d37d699a988fc852b3f177ab106f049ed
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-24 21:32:16 +0300

    Fixed bug

--- frontend/onied/src/App.jsx
+++ frontend/onied/src/App.jsx
@@ -14,6 +14,7 @@ import LoginService from "./services/loginService";
 import ConfirmEmail from "./pages/accountManagement/confirmEmail/confirmEmail";
 
 function App() {
+  LoginService.initialize();
   LoginService.registerAutomaticRefresh();
   return (
     <>
