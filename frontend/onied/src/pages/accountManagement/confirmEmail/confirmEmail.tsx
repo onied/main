30ca827b2c3e44eeb27a779e1952e8407b75ea04
antonborisov571 <antonborisov571@gmail.com>
2024-03-24 18:25:47 +0300

    Added a mail confirmation page

---
+++ frontend/onied/src/pages/accountManagement/confirmEmail/confirmEmail.tsx
@@ -0,0 +1,13 @@
+import classes from "./confirmEmail.module.css";
+import ConfirmEmailComponent from "../../../components/accountManagement/confirmEmail/confirmEmail";
+
+
+function ConfirmEmail() {
+  return (
+    <div className={classes.container}>
+      <ConfirmEmailComponent></ConfirmEmailComponent>
+    </div>
+  );
+}
+
+export default ConfirmEmail;
