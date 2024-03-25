1b04fca84a723a5846959f65cdb8aba9eb96f845
iigaz <125541799+iigaz@users.noreply.github.com>
2024-03-26 01:07:34 +0300

    Apply suggestions from code review

--- backend/Onied/Users/Controllers/ProfileController.cs
+++ backend/Onied/Users/Controllers/ProfileController.cs
@@ -10,29 +10,21 @@ namespace Users.Controllers;
 [Route("api/v1/[controller]")]
 public class ProfileController : ControllerBase
 {
-    private readonly AppDbContext _context;
     private readonly ILogger<ProfileController> _logger;
     private readonly IMapper _mapper;
 
-    public ProfileController(AppDbContext context, ILogger<ProfileController> logger, IMapper mapper)
+    public ProfileController(ILogger<ProfileController> logger, IMapper mapper)
     {
-        _context = context;
         _logger = logger;
         _mapper = mapper;
     }
 
-    [HttpGet]
-    public async Task<ActionResult<UserProfileDto>> Get([FromServices] SignInManager<AppUser> signInManager)
+   [HttpGet]
+    public async Task<ActionResult<UserProfileDto>> Get([FromServices] UserManager<AppUser> userManager)
     {
-        var currentUser = HttpContext.User;
-
-        if (!signInManager.IsSignedIn(currentUser))
-        {
+        var user = await userManager.GetUserAsync(User);
+        if (user == null)
             return Unauthorized();
-        }
-        
-        var userId = currentUser.FindFirstValue(ClaimTypes.NameIdentifier);
-        var user = await _context.FindAsync<AppUser>(userId);
         var userProfile = _mapper.Map<UserProfileDto>(user);
 
         return Ok(userProfile);
