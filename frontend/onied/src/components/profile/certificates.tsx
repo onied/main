d50dd7e9bdc78c1b62408fe041f043849c334da4
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-26 14:53:54 +0300

    Added certificate list with stubs

--- frontend/onied/src/components/profile/certificates.tsx
+++ frontend/onied/src/components/profile/certificates.tsx
@@ -1,12 +1,27 @@
 import classes from "./profile.module.css";
 import { useProfile } from "../../hooks/profile/useProfile";
+import CertificateContainer, { Certificate } from "./certificate/certificate";
+import { useState } from "react";
 
 function ProfileCertificates() {
   const profile = useProfile();
+  const [certificateList, setCertificateList] = useState<
+    Array<Certificate> | undefined
+  >();
   if (profile == null) return <></>;
   return (
     <div className={classes.pageWrapper}>
       <h3 className={classes.pageTitle}>Доступные сертификаты</h3>
+      {certificateList ? (
+        certificateList.map((cert, index) => (
+          <CertificateContainer
+            certificate={cert}
+            key={index}
+          ></CertificateContainer>
+        ))
+      ) : (
+        <p className={classes.noCourses}>Нет доступных сертификатов</p>
+      )}
     </div>
   );
 }
