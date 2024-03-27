aaaf9bb1832129ea7504c558ba402f836fba4c83
Matvey <enhisir@localhost.localdomain>
2024-03-27 15:29:02 +0300

    added services

--- backend/Onied/Courses/Program.cs
+++ backend/Onied/Courses/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddCors();
 
 builder.Services.AddScoped<ICheckTasksService, CheckTasksService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IBlockRepository, BlockRepository>();
 
 var app = builder.Build();
 
