b3d77dca3eff6a28fbae641edea2c3df1a76281a
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-03-30 23:08:12 +0300

    Added basic drag and drop... and it kinda works...
    but not quite as fluid as I'd like it to be...

--- frontend/onied/src/App.tsx
+++ frontend/onied/src/App.tsx
@@ -19,6 +19,7 @@ import ConfirmEmail from "./pages/accountManagement/confirmEmail/confirmEmail";
 import ProfileService from "./services/profileService";
 import { LoadingContext } from "./hooks/profile/loadingContext";
 import TeachingPage from "./pages/teaching/teaching";
+import EditCourseHierarchy from "./pages/editCourse/editHierarchy/editHierarchy";
 
 function App() {
   const [profile, setProfile] = useState<Profile | null>(null);
@@ -42,6 +43,10 @@ function App() {
               <Route
                 path="/course/:courseId/learn/*"
                 element={<Course />}
+              ></Route>
+              <Route
+                path="/course/:courseId/edit/hierarchy"
+                element={<EditCourseHierarchy />}
               ></Route>
               <Route path="/course/:courseId" element={<Preview />}></Route>
               <Route path="/catalog" element={<Catalog />}></Route>
---
+++ frontend/onied/src/pages/editCourse/editHierarchy/editHierarchy.tsx
@@ -0,0 +1,192 @@
+import { useState, useEffect, useCallback } from "react";
+import { Link, useParams } from "react-router-dom";
+import api from "../../../config/axios";
+import classes from "./editHierarchy.module.css";
+import Button from "../../../components/general/button/button";
+import { BeatLoader } from "react-spinners";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import type { DropResult } from "@hello-pangea/dnd";
+
+type Block = {
+  id: number;
+  title: string;
+  blockType: number;
+};
+
+type Module = {
+  id: number;
+  title: string;
+  blocks: Array<Block>;
+};
+
+type Course = {
+  id: number;
+  modules: Array<Module>;
+  title: string;
+};
+
+function EditCourseHierarchy() {
+  const { courseId } = useParams();
+  const [hierarchy, setHierarchy] = useState<Course | null | undefined>();
+  const notFound = <h1 style={{ margin: "3rem" }}>Курс не найден.</h1>;
+  const id = Number(courseId);
+  const blockTypes = ["", "summary", "video", "tasks"];
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+    if (result.draggableId.startsWith("module")) {
+      if (result.destination.droppableId != "hierarchy") {
+        console.log("fuck you");
+        return;
+      }
+      const newArray = Array.from(hierarchy!.modules);
+      const [removed] = newArray.splice(result.source.index, 1);
+      newArray.splice(result.destination.index, 0, removed);
+      setHierarchy({ ...hierarchy!, modules: newArray });
+    } else if (result.draggableId.startsWith("block")) {
+      if (!result.destination.droppableId.startsWith("module")) {
+        return;
+      }
+      const newArrayModules = Array.from(hierarchy!.modules);
+      const fromModule = newArrayModules.findIndex(
+        (value) =>
+          value.id == Number(result.source.droppableId.replace("module", ""))
+      );
+      const toModule = newArrayModules.findIndex(
+        (value) =>
+          value.id ==
+          Number(result.destination!.droppableId.replace("module", ""))
+      );
+      const [removed] = newArrayModules[fromModule].blocks.splice(
+        result.source.index,
+        1
+      );
+      newArrayModules[toModule].blocks.splice(
+        result.destination.index,
+        0,
+        removed
+      );
+      console.log(newArrayModules);
+      setHierarchy({ ...hierarchy!, modules: newArrayModules });
+    }
+  };
+
+  const renderBlock = (block: Block, index: number, moduleIndex: number) => {
+    return (
+      <Draggable key={block.id} draggableId={"block" + block.id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            className={classes.block}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {moduleIndex + 1}.{index + 1}. {block.title}
+          </div>
+        )}
+      </Draggable>
+    );
+  };
+
+  const renderModule = (module: Module, index: number) => {
+    return (
+      <div className={classes.moduleContainer} key={module.id}>
+        <Draggable draggableId={"module" + module.id} index={index}>
+          {(provided: any, snapshot: any) => (
+            <div
+              className={classes.module}
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              {index + 1}. {module.title}
+            </div>
+          )}
+        </Draggable>
+        <Droppable droppableId={"module" + module.id}>
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className={classes.blockList}
+            >
+              {module.blocks.map((block, bi) => renderBlock(block, bi, index))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    if (isNaN(id)) {
+      setHierarchy(null);
+      return;
+    }
+    api
+      .get("courses/" + id + "/hierarchy/")
+      .then((response) => {
+        console.log(response.data);
+        setHierarchy(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+
+        if ("response" in error && error.response.status == 404) {
+          setHierarchy(null);
+        }
+      });
+  }, [id]);
+
+  if (isNaN(id)) {
+    console.log(id);
+    console.log(courseId);
+    return notFound;
+  }
+
+  if (hierarchy === undefined)
+    return (
+      <BeatLoader
+        color="var(--accent-color)"
+        style={{ margin: "3rem" }}
+      ></BeatLoader>
+    );
+  if (hierarchy === null) return notFound;
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.pageHeaderContainer}>
+        <Link to={"/courses/" + courseId + "/edit"} className={classes.back}>
+          ⟵ к редактированию превью
+        </Link>
+        <h1 className={classes.title}>{hierarchy!.title}</h1>
+      </div>
+      <div className={classes.pageBodyContainer}>
+        <div className={classes.buttonsUp}>
+          <Button>добавить модуль</Button>
+        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="hierarchy">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={classes.moduleList}
+              >
+                {hierarchy!.modules.map(renderModule)}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        <div className={classes.buttonsDown}>
+          <Button>сохранить</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default EditCourseHierarchy;
