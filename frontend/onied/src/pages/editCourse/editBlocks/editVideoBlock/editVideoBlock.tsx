de3c1c09ec229ac7d5c9c84f39a73694fd76460e
antonborisov571 <antonborisov571@gmail.com>
2024-03-31 20:22:57 +0300

    Added redirect if user not unauthorized

--- frontend/onied/src/pages/editCourse/editBlocks/editVideoBlock/editVideoBlock.tsx
+++ frontend/onied/src/pages/editCourse/editBlocks/editVideoBlock/editVideoBlock.tsx
@@ -1,7 +1,11 @@
 import classes from "./editVideoBlock.module.css";
 import EditVideoBlockComponent from "../../../../components/editCourse/editBlocks/editVideoBlock/editVideoBlock";
+import { useProfile } from "../../../../hooks/profile/useProfile";
+import { Navigate } from "react-router-dom";
 
 function EditVideoBlock() {
+  const [profile, loading] = useProfile();
+  if (profile == null && !loading) return <Navigate to="/login"></Navigate>;
   return (
     <>
       <div className={classes.container}>
