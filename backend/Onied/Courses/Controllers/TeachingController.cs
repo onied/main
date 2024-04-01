45bebd1b8b87c383d96169b9ae88f21d893a6f9d
antonborisov571 <antonborisov571@gmail.com>
2024-04-02 01:05:51 +0300

    Added TeachingController while moderated sends the same thing as authored

---
+++ backend/Onied/Courses/Controllers/TeachingController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Courses.Dtos;
+using Courses.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Courses.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]/{id:guid}")]
+public class TeachingController(
+    UserRepository userRepository,
+    IMapper mapper
+    ) : ControllerBase
+{
+    [HttpGet]
+    [Route("authored")]
+    public async Task<ActionResult<List<CourseCardDto>>> GetAuthoredCourses(Guid id)
+    {
+        var user = await userRepository.GetUserWithTeachingCoursesAsync(id);
+        if (user is null) return NotFound();
+
+        return mapper.Map<List<CourseCardDto>>(user.Courses);
+    }
+
+    [HttpGet]
+    [Route("moderated")]
+    public async Task<ActionResult<List<CourseCardDto>>> GetModeratedCourses(Guid id)
+    {
+        var user = await userRepository.GetUserWithTeachingCoursesAsync(id);
+        if (user is null) return NotFound();
+
+        return mapper.Map<List<CourseCardDto>>(user.Courses);
+    }
+}
