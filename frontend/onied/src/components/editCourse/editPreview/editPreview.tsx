14c3ee9880419e3eefdc827bbd4a04a50e135f7b
Emin <emin.sarux2004@yandex.ru>
2024-04-02 12:44:58 +0300

    finished markup

--- frontend/onied/src/components/editCourse/editPreview/editPreview.tsx
+++ frontend/onied/src/components/editCourse/editPreview/editPreview.tsx
@@ -1,10 +1,11 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import api from "../../../config/axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import classes from "../editPreview/editPreview.module.css";
 import Button from "../../general/button/button";
 import InputForm from "../../general/inputform/inputform";
 import InputFormArea from "../../general/inputform/inputFormArea";
+import Checkbox from "../../general/checkbox/checkbox";
 
 type PreviewDto = {
   title: string;
@@ -31,6 +32,7 @@ type Errors = {
 };
 
 function EditPreviewComponent() {
+  const navigator = useNavigate();
   const { courseId } = useParams();
   const [previewInfo, setPreview] = useState<PreviewDto | undefined>();
   const [errors, setErrors] = useState<Errors>({
@@ -98,7 +100,6 @@ function EditPreviewComponent() {
               <div className={classes.fixedHeightTextArea}>
                 <InputFormArea
                   id="preview_description"
-                  type="area"
                   value={previewInfo?.description}
                   onChange={(e: ChangeEvent<HTMLInputElement>) =>
                     setPreview({ ...previewInfo!, description: e.target.value })
@@ -126,6 +127,63 @@ function EditPreviewComponent() {
                 {errors.Category ? <div>{errors.Category}</div> : <></>}
               </div>
             </div>
+            <div className={classes.inputChangesTwoColumns}>
+              <div className={classes.inputChangesBlock}>
+                <label className={classes.formLabel} htmlFor="preview_price">
+                  Цена
+                </label>
+                <label
+                  className={classes.formFieldDescription}
+                  htmlFor="preview_price"
+                >
+                  Введите 0, если хотите сделать курс бесплатным для всех
+                </label>
+                <div className={classes.fixedHeightTextArea}>
+                  <InputForm
+                    id="preview_price"
+                    value={previewInfo?.price}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      setPreview({
+                        ...previewInfo!,
+                        price: Number(e.target.value),
+                      })
+                    }
+                  ></InputForm>
+                  {errors.Price ? <div>{errors.Price}</div> : <></>}
+                </div>
+              </div>
+              <div className={classes.inputChangesBlock}>
+                <label
+                  className={classes.formLabel}
+                  htmlFor="preview_complete_time"
+                >
+                  Время прохождения
+                </label>
+                <label
+                  className={classes.formFieldDescription}
+                  htmlFor="preview_complete_time"
+                >
+                  Оценка времени, необходимого для прождения курса
+                </label>
+                <div className={classes.fixedHeightTextArea}>
+                  <InputForm
+                    id="preview_complete_time"
+                    value={previewInfo?.hoursCount}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      setPreview({
+                        ...previewInfo!,
+                        hoursCount: Number(e.target.value),
+                      })
+                    }
+                  ></InputForm>
+                  {errors.CompleteTime ? (
+                    <div>{errors.CompleteTime}</div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         </form>
         <div className={classes.previewPrompt}>
@@ -141,6 +199,77 @@ function EditPreviewComponent() {
         </div>
       </div>
       <div className={classes.line}></div>
+      <div className={classes.toggleChanges}>
+        <div className={classes.toggleChangeWrapper}>
+          <div className={classes.checkboxAndTitle}>
+            <Checkbox id="is-program-visible-checkbox" />
+            <label
+              className={classes.formLabel}
+              htmlFor="is-program-visible-checkbox"
+            >
+              Содержание курса
+            </label>
+          </div>
+          <label
+            className={classes.formFieldDescription}
+            htmlFor="is-program-visible-checkbox"
+          >
+            Содержание курса доступно на превью курса и показывает
+            предварительную программу, составленную из списка модулей
+          </label>
+        </div>
+        <div className={classes.toggleChangeWrapper}>
+          <div className={classes.checkboxAndTitle}>
+            <Checkbox id="has-certificates-checkbox" />
+            <label
+              className={classes.formLabel}
+              htmlFor="has-certificates-checkbox"
+            >
+              Выдача сертификатов
+            </label>
+          </div>
+
+          <label
+            className={classes.formFieldDescription}
+            htmlFor="has-certificates-checkbox"
+          >
+            Выдача сертификатов достпуна только с полной подпиской
+          </label>
+        </div>
+        <div className={classes.toggleChangeWrapper}>
+          <div className={classes.checkboxAndTitle}>
+            <Checkbox id="is-archived-checkbox" />
+            <label className={classes.formLabel} htmlFor="is-archived-checkbox">
+              <p>Заархивировать курс</p>
+            </label>
+          </div>
+
+          <label
+            className={classes.formFieldDescription}
+            htmlFor="is-archived-checkbox"
+          >
+            Курс может уйти в архив, тогда он будет недоступен для покупки
+            (ученик может не платить за него, он считается бесплатным).
+          </label>
+        </div>
+      </div>
+      <div className={classes.line}></div>
+      <div className={classes.footerButtons}>
+        <Button>сохранить изменения</Button>
+        <Link to={`../course/${courseId}/edit/hierarchy`}>
+          <Button
+            style={{
+              boxShadow: "none",
+              backgroundColor: "white",
+              border: "3px solid var(--button-color)",
+              cursor: "inherit",
+              color: "var(--accent-color)",
+            }}
+          >
+            редактировать содержимое курса
+          </Button>
+        </Link>
+      </div>
     </>
   );
 }
