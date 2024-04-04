099bd12908634c05e79a43ef24165f61fbc894a9
antonborisov571 <antonborisov571@gmail.com>
2024-04-04 10:44:27 +0300

    Added ModuleRepository

---
+++ backend/Onied/Courses/Services/IModuleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Courses.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace Courses.Services;
+
+public interface IModuleRepository
+{
+    public Task<Module?> GetModuleAsync(int id);
+    public Task AddModuleAsync(Module module);
+    public Task<int> AddModuleReturnIdAsync(Module module);
+    public Task UpdateModuleAsync(Module module);
+}
---
+++ backend/Onied/Courses/Services/ModuleRepository.cs
@@ -0,0 +1,32 @@
+﻿using Courses.Models;
+using Microsoft.EntityFrameworkCore;
+using Task = System.Threading.Tasks.Task;
+
+namespace Courses.Services;
+
+public class ModuleRepository(AppDbContext dbContext) : IModuleRepository
+{
+    public Task<Module?> GetModuleAsync(int id) =>
+        dbContext.Modules
+            .Include(m => m.Blocks)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task AddModuleAsync(Module module)
+    {
+        await dbContext.Modules.AddAsync(module);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<int> AddModuleReturnIdAsync(Module module)
+    {
+        await dbContext.Modules.AddAsync(module);
+        await dbContext.SaveChangesAsync();
+        return module.Id;
+    }
+
+    public async Task UpdateModuleAsync(Module module)
+    {
+        dbContext.Modules.Update(module);
+        await dbContext.SaveChangesAsync();
+    }
+}
