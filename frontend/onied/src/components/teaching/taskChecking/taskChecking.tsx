5ced7d087c162d584e9488a3a99f7bac9c10c54a
Emin <emin.sarux2004@yandex.ru>
2024-04-06 15:58:59 +0300

    added styles but they broke

--- frontend/onied/src/components/teaching/taskChecking/taskChecking.tsx
+++ frontend/onied/src/components/teaching/taskChecking/taskChecking.tsx
@@ -2,8 +2,10 @@ import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Arrow from "../../../assets/arrow.svg";
+import "./muiAccordionOverride.css";
 import classes from "./taskChecking.module.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function TaskChecking() {
   const [coursesWithTasksList, setTaskList] = useState<Array<CourseWithTasks>>([
@@ -12,16 +14,28 @@ function TaskChecking() {
       courseName: "Название курса.",
       tasksToCheck: [
         {
+          taskId: 1,
           moduleName: "Первый модуль",
           blockName: "Первый блок",
           taskTitle: "Напишите эссе",
-          isChecked: false,
         },
         {
+          taskId: 2,
           moduleName: "Первый модуль",
           blockName: "Первый блок",
           taskTitle: "Напишите эссе",
-          isChecked: false,
+        },
+        {
+          taskId: 3,
+          moduleName: "Первый модуль",
+          blockName: "Первый блок",
+          taskTitle: "Напишите эссе",
+        },
+        {
+          taskId: 4,
+          moduleName: "Первый модуль",
+          blockName: "Первый блок",
+          taskTitle: "Напишите эссе",
         },
       ],
     },
@@ -30,29 +44,58 @@ function TaskChecking() {
       courseName: "Название курса. 2",
       tasksToCheck: [
         {
+          taskId: 5,
           moduleName: "Первый модуль",
           blockName: "Первый блок",
           taskTitle: "Напишите эссе",
-          isChecked: false,
         },
         {
+          taskId: 6,
           moduleName: "Первый модуль",
           blockName: "Первый блок",
           taskTitle: "Напишите эссе",
-          isChecked: false,
+        },
+      ],
+    },
+    {
+      courseId: 3,
+      courseName: "Название курса. 3",
+      tasksToCheck: [
+        {
+          taskId: 7,
+          moduleName: "Первый модуль",
+          blockName: "Первый блок",
+          taskTitle: "Напишите эссе",
+        },
+        {
+          taskId: 8,
+          moduleName: "Первый модуль",
+          blockName: "Первый блок",
+          taskTitle: "Напишите эссе",
         },
       ],
     },
   ]);
 
   return (
-    <div>
+    <div className={classes.accordionsWrapper}>
       {coursesWithTasksList.map((courseWithTask) => (
-        <Accordion>
-          <AccordionSummary>{courseWithTask.courseName}</AccordionSummary>
+        <Accordion className={classes.accordion}>
+          <AccordionSummary
+            className={classes.accordionSummary}
+            expandIcon={<img src={Arrow} />}
+          >
+            <p>{courseWithTask.courseName}</p>
+          </AccordionSummary>
           {courseWithTask.tasksToCheck.map((task) => (
-            <AccordionDetails>
+            <AccordionDetails className={classes.accordionDetails}>
               <TaskDescription {...task}></TaskDescription>
+              <Link
+                to={"/check/" + task.taskId}
+                className={classes.checkButton}
+              >
+                проверить
+              </Link>
             </AccordionDetails>
           ))}
         </Accordion>
@@ -64,7 +107,7 @@ function TaskChecking() {
 function TaskDescription(props: TaskToCheck) {
   return (
     <div className={classes.taskWrapper}>
-      <div>{props.taskTitle}</div>
+      <div className={classes.taskName}>{props.taskTitle}</div>
       <div className={classes.moduleAndBlockName}>
         <span>{props.moduleName}</span>
         <img src={Arrow} />
@@ -81,10 +124,10 @@ type CourseWithTasks = {
 };
 
 type TaskToCheck = {
+  taskId: number;
   moduleName: string;
   blockName: string;
   taskTitle: string;
-  isChecked: boolean;
 };
 
 export default TaskChecking;
