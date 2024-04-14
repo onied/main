7685432ec04db667313a6c45523d100ec028e1c5
Matvey <enhisir@localhost.localdomain>
2024-04-14 23:35:02 +0300

    added restrictions

--- backend/Onied/Purchases.Data/Models/Course.cs
+++ backend/Onied/Purchases.Data/Models/Course.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Purchases.Data.Models;
 
 public class Course
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+
+    [MinLength(1)]
+    [MaxLength(200)]
+    public string Title { get; set; } = null!;
+
+    [Range(0, 1000000)]
     public decimal Price { get; set; }
     public bool HasCertificates { get; set; }
 
--- backend/Onied/Purchases.Data/Models/Subscription.cs
+++ backend/Onied/Purchases.Data/Models/Subscription.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Purchases.Data.Models;
 
 public class Subscription
@@ -6,7 +8,11 @@ public class Subscription
     public bool CoursesHighlightingEnabled { get; set; }
     public bool AdsEnabled { get; set; }
     public bool CertificatesEnabled { get; set; }
+
+    [Range(-1, int.MaxValue)]
     public int ActiveCoursesNumber { get; set; }
+
+    [Range(0, 1000000)]
     public decimal Price { get; set; }
 
     public List<User> Users { get; set; } = null!;
