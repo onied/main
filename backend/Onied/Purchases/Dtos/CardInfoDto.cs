1d2d7f381d2bf8584e92c889623e6937c6bad73d
Matvey <enhisir@localhost.localdomain>
2024-04-15 11:41:47 +0300

    added dtos, changed card cvc type

---
+++ backend/Onied/Purchases/Dtos/CardInfoDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Purchases.Dtos;
+
+public class CardInfoDto
+{
+    [RegularExpression(@"^[0-9]{13,19}$")]
+    public string Number { get; set; } = null!;
+
+    [RegularExpression(@"^[A-Z\s\-]+$")]
+    public string Holder { get; set; } = null!;
+
+    [Range(1, 12)]
+    public int Month;
+
+    [Range(0, 99)]
+    public int Year { get; set; }
+
+    [RegularExpression(@"^[0-9]{3}$")]
+    public string SecurityCode { get; set; } = null!;
+}
--- backend/Onied/Purchases/Dtos/PurchaseRequestDto.cs
+++ backend/Onied/Purchases/Dtos/PurchaseRequestDto.cs
@@ -4,7 +4,7 @@ namespace Purchases.Dtos;
 
 public class PurchaseRequestDto
 {
-    public string Title { get; set; } = null!;
-    public decimal Price { get; set; }
     public PurchaseType PurchaseType { get; set; }
+    public decimal Price { get; set; }
+    public CardInfoDto CardInfo { get; set; } = null!;
 }
