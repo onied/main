255cb0615969fd947590c91f3b6eede77c1eac5b
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-04-15 21:48:37 +0300

    Added get course method

--- backend/certificates/src/course/course.service.spec.ts
+++ backend/certificates/src/course/course.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CourseService } from "./course.service";
+import { Course } from "./course.entity";
+import { Repository } from "typeorm";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "src/user/user.entity";
 
 describe("CourseService", () => {
   let service: CourseService;
+  let repo: Repository<Course>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CourseService],
+      providers: [
+        CourseService,
+        {
+          provide: getRepositoryToken(Course),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<CourseService>(CourseService);
+    repo = module.get<Repository<Course>>(getRepositoryToken(Course));
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should return for findOne", async () => {
+    // Arrange
+
+    const author: User = {
+      id: "066c23b2-cabf-4a7f-a6cd-e2c8b9e92425",
+      firstName: "Василий",
+      lastName: "Теркин",
+      avatar:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Lynx_lynx-4.JPG/640px-Lynx_lynx-4.JPG",
+    };
+
+    const course: Course = {
+      id: 1,
+      title: "asdfasdf",
+      author: author,
+      hasCertificates: true,
+    };
+    jest.spyOn(repo, "findOneBy").mockResolvedValueOnce(course);
+
+    // Act
+
+    const result = await service.findOne(course.id);
+
+    // Assert
+
+    expect(result).toEqual(course);
   });
 });
--- backend/certificates/src/course/course.service.ts
+++ backend/certificates/src/course/course.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Course } from "./course.entity";
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>
+  ) {}
+
+  findOne(id: number): Promise<Course | null> {
+    return this.courseRepository.findOneBy({ id });
+  }
+}
