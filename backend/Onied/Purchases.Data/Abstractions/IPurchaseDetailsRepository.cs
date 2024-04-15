7c3efc20994aec37e46452d8567896bed042c821
Matvey <enhisir@localhost.localdomain>
2024-04-15 09:17:30 +0300

    added migrations

--- backend/Onied/Purchases.Data/Abstractions/IPurchaseDetailsRepository.cs
+++
@@ -1,8 +0,0 @@
-using Purchases.Data.Models.PurchaseDetails;
-
-namespace Purchases.Data.Abstractions;
-
-public class IPurchaseDetailsRepository
-{
-    public Task<PurchaseDetails?>
-}
--- backend/Onied/Purchases/Program.cs
+++ backend/Onied/Purchases/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Purchases.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+    optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("PurchasesDatabase"),
+        b => b.MigrationsAssembly("Purchases"))
+        .UseSnakeCaseNamingConvention());
 builder.Services.AddRepositories();
 
 var app = builder.Build();
