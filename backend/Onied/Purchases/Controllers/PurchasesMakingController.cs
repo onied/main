1160f2371d1e880526eab0b5cb61338edb5173a1
Matvey <enhisir@localhost.localdomain>
2024-04-16 16:45:30 +0300

    added certificate purchase making

--- backend/Onied/Purchases/Controllers/PurchasesMakingController.cs
+++ backend/Onied/Purchases/Controllers/PurchasesMakingController.cs
@@ -65,4 +65,40 @@ public class PurchasesMakingController(
         await purchaseCreatedProducer.PublishAsync(purchase);
         return Results.Ok();
     }
+
+    [HttpPost("certificate/{courseId:int}")]
+    public async Task<IResult> MakeCertificatePurchase(int courseId, [FromBody] PurchaseRequestDto dto, Guid userId)
+    {
+        dto = dto with { UserId = userId };
+        if (dto.PurchaseType is not PurchaseType.Certificate
+            || dto.CourseId is null) return Results.BadRequest();
+
+        var user = await userRepository.GetAsync(dto.UserId!.Value, true);
+        var course = await courseRepository.GetAsync(dto.CourseId!.Value);
+        if (user is null || course is null) return Results.NotFound(); // validation service
+
+        if (!course.HasCertificates) return Results.Forbid();
+
+        var maybeAlreadyBought = user.Purchases
+            .SingleOrDefault(p => p.PurchaseDetails.PurchaseType is PurchaseType.Certificate
+                                  && (p.PurchaseDetails as CertificatePurchaseDetails)!.CourseId == courseId);
+        if (maybeAlreadyBought is not null)
+            return Results.Forbid();
+
+        var purchase = mapper.Map<Purchase>(dto);
+
+        const decimal defaultCertificatePrice = 1000;
+        if (purchase.Price != defaultCertificatePrice) return Results.BadRequest(); // price check service
+        var purchaseDetails = new CoursePurchaseDetails()
+        {
+            PurchaseType = PurchaseType.Certificate,
+            CourseId = dto.CourseId!.Value,
+        };
+
+        purchase = await purchaseRepository.AddAsync(purchase, purchaseDetails);
+        purchase.Token = tokenService.GetToken(purchase);
+        await purchaseRepository.UpdateAsync(purchase);
+        await purchaseCreatedProducer.PublishAsync(purchase);
+        return Results.Ok();
+    }
 }
