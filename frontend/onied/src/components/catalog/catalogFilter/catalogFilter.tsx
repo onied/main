f421b9983063fccd6a3435bdf8dde80ddc2b59ab
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-04-19 15:52:27 +0300

    Added search params

--- frontend/onied/src/components/catalog/catalogFilter/catalogFilter.tsx
+++ frontend/onied/src/components/catalog/catalogFilter/catalogFilter.tsx
@@ -4,6 +4,7 @@ import InputForm from "../../general/inputform/inputform";
 import Select from "../../general/inputform/select";
 import classes from "./catalogFilter.module.css";
 import api from "../../../config/axios";
+import { useSearchParams } from "react-router-dom";
 
 type CategoryDto = {
   id: number;
@@ -11,6 +12,7 @@ type CategoryDto = {
 };
 
 function CatalogFilter() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [categories, setCategories] = useState<
     Array<CategoryDto> | undefined
   >();
@@ -19,8 +21,8 @@ function CatalogFilter() {
   const [priceTo, setPriceTo] = useState<number | undefined>();
   const [timeFrom, setTimeFrom] = useState<number | undefined>();
   const [timeTo, setTimeTo] = useState<number | undefined>();
-  const [certificatesOnly, setCertificatesOnly] = useState<boolean>(true);
-  const [activeOnly, setActiveOnly] = useState<boolean>(true);
+  const [certificatesOnly, setCertificatesOnly] = useState<boolean>(false);
+  const [activeOnly, setActiveOnly] = useState<boolean>(false);
 
   const setNumberOrUndefined = (
     event: ChangeEvent<HTMLInputElement>,
@@ -30,6 +32,70 @@ function CatalogFilter() {
     if (event.target.value.length == 0) setValue(undefined);
     else setValue(number);
   };
+
+  useEffect(() => {
+    const category = searchParams.get("category");
+    const priceFrom = searchParams.get("priceFrom");
+    const priceTo = searchParams.get("priceTo");
+    const timeFrom = searchParams.get("timeFrom");
+    const timeTo = searchParams.get("timeTo");
+    const certificatesOnly = searchParams.get("certificatesOnly");
+    const activeOnly = searchParams.get("activeOnly");
+
+    const setNumberIfExists = (
+      param: string | null,
+      setValue: (value: number | undefined) => void
+    ) => {
+      if (param == null) setValue(undefined);
+      else setValue(Number(param));
+    };
+
+    const setBooleanIfExists = (
+      param: string | null,
+      setValue: (value: boolean) => void
+    ) => {
+      if (param == null) setValue(false);
+      else setValue(param.toLocaleLowerCase() == "true");
+    };
+
+    setNumberIfExists(category, setCategoryId);
+    setNumberIfExists(priceFrom, setPriceFrom);
+    setNumberIfExists(priceTo, setPriceTo);
+    setNumberIfExists(timeFrom, setTimeFrom);
+    setNumberIfExists(timeTo, setTimeTo);
+    setBooleanIfExists(certificatesOnly, setCertificatesOnly);
+    setBooleanIfExists(activeOnly, setActiveOnly);
+  }, [searchParams]);
+
+  useEffect(() => {
+    console.log("here");
+    if (categoryId !== undefined)
+      searchParams.set("category", categoryId.toString());
+    else searchParams.delete("category");
+    if (priceFrom !== undefined)
+      searchParams.set("priceFrom", priceFrom.toString());
+    else searchParams.delete("priceFrom");
+    if (priceTo !== undefined) searchParams.set("priceTo", priceTo.toString());
+    else searchParams.delete("priceTo");
+    if (timeFrom !== undefined)
+      searchParams.set("timeFrom", timeFrom.toString());
+    else searchParams.delete("timeFrom");
+    if (timeTo !== undefined) searchParams.set("timeTo", timeTo.toString());
+    else searchParams.delete("timeTo");
+    if (certificatesOnly) searchParams.set("certificatesOnly", "true");
+    else searchParams.delete("certificatesOnly");
+    if (activeOnly) searchParams.set("activeOnly", "true");
+    else searchParams.delete("activeOnly");
+    setSearchParams(searchParams);
+  }, [
+    categoryId,
+    priceFrom,
+    priceTo,
+    timeFrom,
+    timeTo,
+    certificatesOnly,
+    activeOnly,
+  ]);
 
   useEffect(() => {
     api
@@ -54,11 +120,13 @@ function CatalogFilter() {
           value={categoryId}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setCategoryId(
-              event.target.value ? Number(event.target.value) : undefined
+              event.target.value !== "any"
+                ? Number(event.target.value)
+                : undefined
             )
           }
         >
-          <option value={undefined}>Любая категория</option>
+          <option value="any">Любая категория</option>
           {categories?.map((categoryDto) => (
             <option key={categoryDto.id} value={categoryDto.id}>
               {categoryDto.name}
