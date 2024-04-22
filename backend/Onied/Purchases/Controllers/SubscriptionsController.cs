e678e6d416756338ad2d7bfa5291303103ed9bb6
antonborisov571 <antonborisov571@gmail.com>
2024-04-22 19:43:47 +0300

    Added SubscriptionsController.cs

---
+++ backend/Onied/Purchases/Controllers/SubscriptionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Purchases.Services.Abstractions;
+
+namespace Purchases.Controllers;
+
+[Route("api/v1/purchases/[controller]")]
+public class SubscriptionsController(
+    ISubscriptionManagementService subscriptionManagementService
+    ) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IResult> GetSubscriptionsByUser(Guid userId)
+    {
+        return await subscriptionManagementService.GetSubscriptionsByUser(userId);
+    }
+}
