0d1ecfec3903fff0ff6d20e8ed5458a3d4ccd3c6
Matvey <enhisir@localhost.localdomain>
2024-04-22 12:27:55 +0300

    added mapper

--- backend/Onied/Purchases/Controllers/SubscriptionsController.cs
+++ backend/Onied/Purchases/Controllers/SubscriptionsController.cs
@@ -1,16 +1,22 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Purchases.Data.Abstractions;
+using Purchases.Dtos;
 
 namespace Purchases.Controllers;
 
 [Route("/api/v1/[controller]")]
-public class SubscriptionsController(IUserRepository userRepository) : ControllerBase
+public class SubscriptionsController(
+    IMapper mapper,
+    IUserRepository userRepository) : ControllerBase
 {
     [HttpGet("active")]
     public async Task<IResult> GetActiveSubscription([FromQuery] Guid userId)
     {
         var user = await userRepository.GetAsync(userId, withSubscription: true);
 
-        return user is null ? Results.NotFound() : Results.Ok(user.Subscription);
+        return user is null
+            ? Results.NotFound()
+            : Results.Ok(mapper.Map<SubscriptionDto>(user.Subscription));
     }
 }
