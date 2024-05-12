6490372090a1246809471a9bb113faa0921e2e66
Matvey <enhisir@localhost.localdomain>
2024-05-12 15:34:33 +0300

    fixed program.cs

--- backend/Onied/Purchases/Controllers/SubscriptionsController.cs
+++ backend/Onied/Purchases/Controllers/SubscriptionsController.cs
@@ -1,28 +1,17 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using Purchases.Data.Abstractions;
+﻿using Microsoft.AspNetCore.Mvc;
 using Purchases.Services.Abstractions;
-using Purchases.Dtos;
 using Purchases.Dtos.Requests;
 
 namespace Purchases.Controllers;
 
 [Route("api/v1/purchases/[controller]")]
 public class SubscriptionsController(
-    IMapper mapper,
-    IUserRepository userRepository,
     ISubscriptionManagementService subscriptionManagementService
 ) : ControllerBase
 {
     [HttpGet("active")]
     public async Task<IResult> GetActiveSubscription([FromQuery] Guid userId)
-    {
-        var user = await userRepository.GetAsync(userId, withSubscription: true);
-
-        return user is null
-            ? Results.NotFound()
-            : Results.Ok(mapper.Map<SubscriptionDto>(user.Subscription));
-    }
+        => await subscriptionManagementService.GetActiveSubscription(userId);
 
     [HttpGet]
     public async Task<IResult> GetSubscriptionsByUser(Guid userId)
