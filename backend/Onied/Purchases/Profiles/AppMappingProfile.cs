f953b305dd4349de4ebe5860e1394ba2b355a35b
Matvey <enhisir@localhost.localdomain>
2024-05-13 17:49:29 +0300

    fixed bug with full sub

--- backend/Onied/Purchases/Profiles/AppMappingProfile.cs
+++ backend/Onied/Purchases/Profiles/AppMappingProfile.cs
@@ -23,11 +23,11 @@ public class AppMappingProfile : Profile
         CreateMap<Subscription, SubscriptionDto>()
             .ForMember(
                 dest => dest.CourseCreatingEnabled,
-                opt => opt.MapFrom(src => src.ActiveCoursesNumber > 0));
+                opt => opt.MapFrom(src => src.ActiveCoursesNumber != 0));
         CreateMap<Subscription, SubscriptionUserDto>()
             .ForMember(
                 dest => dest.CourseCreatingEnabled,
-                opt => opt.MapFrom(src => src.ActiveCoursesNumber > 0));
+                opt => opt.MapFrom(src => src.ActiveCoursesNumber != 0));
 
         CreateMap<SubscriptionPurchaseDetails, PurchaseDetailsDto>();
 
