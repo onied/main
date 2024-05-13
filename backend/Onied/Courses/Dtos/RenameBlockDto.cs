c05aaa24a9febe76e8b0b256bc422e728df33532
antonborisov571 <antonborisov571@gmail.com>
2024-05-14 00:51:07 +0300

    Added RenameBlockDto.cs

---
+++ backend/Onied/Courses/Dtos/RenameBlockDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Courses.Dtos;
+
+public class RenameBlockDto
+{
+    public int BlockId { get; set; }
+
+    [MinLength(1)]
+    [MaxLength(200)]
+    public string Title { get; set; } = null!;
+}
