db3f63c21495e4d6b2d918a12805eccd0cc89021
Matvey <enhisir@localhost.localdomain>
2024-05-14 11:40:57 +0300

    fixed purchases beatloader

--- frontend/onied/src/components/purchase/coursePurchase/coursePurchase.tsx
+++ frontend/onied/src/components/purchase/coursePurchase/coursePurchase.tsx
@@ -11,8 +11,8 @@ import {
 } from "../../../types/purchase";
 import { useEffect, useState } from "react";
 import api from "../../../config/axios";
-import { BeatLoader } from "react-spinners";
 import NotFound from "../../general/responses/notFound/notFound";
+import CustomBeatLoader from "../../general/customBeatLoader";
 
 function CoursePurchase() {
   const navigate = useNavigate();
@@ -40,7 +40,7 @@ function CoursePurchase() {
       });
   }, []);
 
-  if (loading) return <BeatLoader color="var(--accent-color)" />;
+  if (loading) return <CustomBeatLoader />;
   if (error == null && purchaseInfo === null)
     return <NotFound>Курс не найден</NotFound>;
 
