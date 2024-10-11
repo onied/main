fbef65adf1c61d297ff1be1c655ff2dc7e6fadfc
Matvey Sergeev <enhisir@localhost.localdomain>
2024-10-11 19:47:43 +0500

    added common validator

--- backend/Onied/MassTransit.Data/Messages/NotificationSent.cs
+++ backend/Onied/MassTransit.Data/Messages/NotificationSent.cs
@@ -1,10 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using Common.Validation.Attributes;
 
 namespace MassTransit.Data.Messages;
 
 public record NotificationSent(
     [MaxLength(100)] string Title,
     [MaxLength(350)] string Message,
-    Guid UserId,
+    [property: GuidNotEmpty(ErrorMessage = "UserId cannot be Guid.Empty")] Guid UserId,
     [Url][MaxLength(2048)] string Image = "https://www.emojiall.com/en/svg-to-png/twitter/1920/1f479.png"
 );
