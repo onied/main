01942d188c5870756958c680f1a01c1f0436180b
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-10-14 17:04:45 +0300

    Fixed test

--- frontend/onied/src/components/blocks/tasks/tasks.spec.tsx
+++ frontend/onied/src/components/blocks/tasks/tasks.spec.tsx
@@ -2,43 +2,59 @@ import "@testing-library/jest-dom";
 import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { http, HttpResponse } from "msw";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 
 import { server } from "@onied/tests/mocks/server";
 import backend from "@onied/tests/helpers/backend";
 import { BlockType, TasksBlock } from "@onied/types/block";
 import Tasks from "./tasks";
+import * as hooks from "@onied/hooks";
+import { TaskType } from "../../../types/task";
 
-describe("Summary", () => {
-    it("renders summary with no file when valid response", async () => {
-        const courseId = 1;
-        const blockId = 1;
-        const url = `courses/${courseId}/summary/${blockId}`;
+describe("Tasks", () => {
+  it("renders tasks with no file when valid response", async () => {
+    const courseId = 1;
+    const blockId = 1;
+    const url = `courses/${courseId}/tasks/${blockId}`;
 
-        const summary: TasksBlock = {
-            id: blockId,
-            title: "default text",
-            blockType: BlockType.SummaryBlock,
-            markdownText: "шаблонный текст",
-            completed: false
-        }
+    const tasks: TasksBlock = {
+      id: blockId,
+      title: "default text",
+      blockType: BlockType.TasksBlock,
+      completed: false,
+      tasks: [
+        {
+          id: 1,
+          title: "string",
+          taskType: TaskType.ManualReview,
+          maxPoints: 5,
+          isNew: false,
+          variants: undefined,
+        },
+      ],
+    };
 
-        server.use(
-            http.get(backend(url), () => {
-                return HttpResponse.json(summary, { status: 200 });
-            })
-        );
+    server.use(
+      http.get(backend(url), () => {
+        return HttpResponse.json(tasks, { status: 200 });
+      }),
+      http.get(backend("courses/1/tasks/1/points"), () => {
+        return HttpResponse.json([]);
+      })
+    );
 
-        // render(
-        //     <MemoryRouter initialEntries={[url]}>
-        //         <Summary courseId={courseId} blockId={blockId} />
-        //     </MemoryRouter>
-        // );
-
-        // await waitFor(() => {
-        //     expect(screen.getByTestId('markdown-summary-title')).toBeInTheDocument();
-        //     expect(screen.queryByTestId('markdown-summary-file-link')).not.toBeInTheDocument();
-        //     expect(screen.getByText(/шаблонный текст/i)).toBeInTheDocument();
-        // });
+    vi.spyOn(hooks, "useAppSelector").mockImplementation((_) => {
+      return { hierarchy: { modules: [{ blocks: [{}] }] } };
     });
-})
+
+    render(
+      <MemoryRouter initialEntries={[url]}>
+        <Tasks courseId={courseId} blockId={blockId} />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText("string")).toBeInTheDocument();
+    });
+  });
+});
