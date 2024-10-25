2ac30e1041bf7cc5ea38931effd2b634848a5d7b
antonborisov571 <antonborisov571@gmail.com>
2024-10-25 19:55:42 +0300

    Added implementations to the controller methods

--- backend/Onied/Support/Support/Controllers/ChatController.cs
+++ backend/Onied/Support/Support/Controllers/ChatController.cs
@@ -1,22 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Support.Abstractions;
 
 namespace Support.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class ChatController : ControllerBase
+public class ChatController(IChatService chatService) : ControllerBase
 {
     [HttpGet]
-    public Task<IResult> GetUserChat(
+    public async Task<IResult> GetUserChat(
         [FromQuery] Guid? userId)
     {
-        return Task.FromResult(Results.Ok(new { UserId = userId.ToString() }));
+        var response = await chatService.GetUserChat(userId);
+        return Results.Ok(response);
     }
 
     [HttpGet]
     [Route("{chatId:guid}")]
-    public Task<IResult> GetChatById([FromRoute] Guid chatId, [FromQuery] Guid? userId)
+    public async Task<IResult> GetChatById([FromRoute] Guid chatId, [FromQuery] Guid? userId)
     {
-        return Task.FromResult(Results.Ok(new { ChatId = chatId.ToString(), UserId = userId.ToString() }));
+        var response = await chatService.GetChatById(chatId, userId);
+        return Results.Ok(response);
     }
 }
