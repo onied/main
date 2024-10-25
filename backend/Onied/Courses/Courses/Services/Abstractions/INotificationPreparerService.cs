56a8fb2cf322db55ee9d3c9feffb9c71d9890623
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-10-25 18:50:34 +0300

    Moved notification preparer service to Notification itself.
    WHY:
    - First of all, it's not really a service, it's behaviour is strictly
    dependent on NotificationSent object and nothing else.
    - All producers of NotificationSent will use these methods anyway.
    - There is no real need to test it, because the only thing it really
    does is to shorten properties to fit them to the database. This
    behaviour strictly depends on the MaxLength of properties, which is
    written in NotificationSent object.
    - Preparer is not even a real word, at least not in the English
    language. The correct one would be prepper [source:
    https://www.merriam-webster.com/dictionary/prepper]

--- backend/Onied/Courses/Courses/Services/Abstractions/INotificationPreparerService.cs
+++
@@ -1,8 +0,0 @@
-using MassTransit.Data.Messages;
-
-namespace Courses.Services.Abstractions;
-
-public interface INotificationPreparerService
-{
-    public NotificationSent PrepareNotification(NotificationSent notification);
-}
--- backend/Onied/MassTransit.Data/Messages/NotificationSent.cs
+++ backend/Onied/MassTransit.Data/Messages/NotificationSent.cs
@@ -6,6 +6,22 @@ namespace MassTransit.Data.Messages;
 public record NotificationSent(
     [MaxLength(100)] string Title,
     [MaxLength(350)] string Message,
-    [property: GuidNotEmpty(ErrorMessage = "UserId cannot be Guid.Empty")] Guid UserId,
+    [property: GuidNotEmpty(ErrorMessage = "UserId cannot be Guid.Empty")]
+    Guid UserId,
     [Url][MaxLength(2048)] string Image = "https://www.emojiall.com/en/svg-to-png/twitter/1920/1f479.png"
-);
+)
+{
+    public static NotificationSent Normalize(NotificationSent notification)
+        => notification with
+        {
+            Title = notification.Title.Length > 100
+                ? string.Concat(notification.Title.AsSpan(0, 97), "...")
+                : notification.Title,
+            Message = notification.Message.Length > 350
+                ? string.Concat(notification.Message.AsSpan(0, 347), "...")
+                : notification.Message,
+            Image = notification.Image.Length > 2048
+                ? "https://www.emojiall.com/en/svg-to-png/twitter/1920/1f479.png"
+                : notification.Image
+        };
+};
