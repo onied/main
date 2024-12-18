9cda1f9eb80c1c2c3ebab16dbe41cb1d75f94f8d
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2024-12-18 11:46:26 +0300

    Forgot to actually use MediatR

--- backend/Onied/Storage/Storage/Controllers/StorageController.cs
+++ backend/Onied/Storage/Storage/Controllers/StorageController.cs
@@ -1,31 +1,34 @@
 using System.Net;
 using System.Text.RegularExpressions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Minio;
 using Minio.DataModel.Args;
 using Minio.Exceptions;
 using Storage.Abstractions;
+using Storage.Commands;
 using Storage.Exceptions;
+using Storage.Queries;
 using Unidecode.NET;
 
 namespace Storage.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class StorageController(IStorageService storageService)
+public class StorageController(ISender sender)
     : ControllerBase
 {
     [HttpPost]
     [Route("upload")]
     public async Task<IResult> Upload(IFormFileCollection files)
     {
-        return await storageService.Upload(files);
+        return await sender.Send(new Upload(files));
     }
 
     [HttpGet]
     [Route("get/{objectName}")]
     public async Task<IResult> Get(string objectName)
     {
-        return await storageService.Get(objectName);
+        return await sender.Send(new Get(objectName));
     }
 }
