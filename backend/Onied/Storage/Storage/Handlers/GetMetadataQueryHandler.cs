0fa1eee315bf77bebaf52e85b21fe55bc07c0a07
antonborisov571 <antonborisov571@gmail.com>
2025-01-13 22:12:30 +0300

    Added handlers

---
+++ backend/Onied/Storage/Storage/Handlers/GetMetadataQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Storage.Abstractions;
+using Storage.Queries;
+
+namespace Storage.Handlers;
+
+public class GetMetadataQueryHandler(IStorageService storageService) : IRequestHandler<GetMetadataByFileIdQuery, IResult>
+{
+    public async Task<IResult> Handle(GetMetadataByFileIdQuery request, CancellationToken cancellationToken)
+    {
+        return await storageService.GetMetadata(request.FileId);
+    }
+}
---
+++ backend/Onied/Storage/Storage/Handlers/GetUrlByFileIdQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Storage.Abstractions;
+using Storage.Queries;
+
+namespace Storage.Handlers;
+
+public class GetUrlByFileIdQueryHandler(IStorageService storageService) : IRequestHandler<GetUrlByFileIdQuery, IResult>
+{
+    public async Task<IResult> Handle(GetMetadataByFileIdQuery request, CancellationToken cancellationToken)
+    {
+        return await storageService.GetMetadata(request.FileId);
+    }
+
+    public async Task<IResult> Handle(GetUrlByFileIdQuery request, CancellationToken cancellationToken)
+    {
+        return await storageService.GetUrlByFileId(request.FileId);
+    }
+}
