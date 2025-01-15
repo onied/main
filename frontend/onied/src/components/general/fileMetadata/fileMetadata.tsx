ab580a92ef128245404236b112e03671817bd850
antonborisov571 <antonborisov571@gmail.com>
2025-01-15 15:44:48 +0300

    Added component fileMetadata

---
+++ frontend/onied/src/components/general/fileMetadata/fileMetadata.tsx
@@ -0,0 +1,37 @@
+﻿import { useEffect, useState } from "react";
+import api from "@onied/config/axios";
+import classes from "./FileMetadata.module.css";
+
+interface Metadata {
+  [key: string]: string;
+}
+
+function FileMetadata(props: { fileId?: string }) {
+  const [metadata, setMetadata] = useState<Metadata | null>(null);
+
+  useEffect(() => {
+    if (!props.fileId) return;
+
+    api
+      .get(`/storage/metadata/${props.fileId}`)
+      .then((response) => setMetadata(response.data))
+      .catch(() => setMetadata(null)); // Suppress errors
+  }, [props.fileId]);
+
+  if (!props.fileId || !metadata) return null;
+
+  return (
+    <div className={classes.metadataContainer}>
+      <h4>Metadata</h4>
+      <ul>
+        {Object.entries(metadata).map(([key, value]) => (
+          <li key={key}>
+            <strong>{key}:</strong> {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FileMetadata;
