b16d40364c2f1b256581acd338efb166c0ebc20c
antonborisov571 <antonborisov571@gmail.com>
2025-04-10 19:34:36 +0300

    Почему ты не закоммитилось...

--- backend/certificates/src/course/course.module.ts
+++ backend/certificates/src/course/course.module.ts
@@ -3,9 +3,10 @@ import { CourseService } from "./course.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Course } from "./course.entity";
 import { User } from "../user/user.entity";
+import { RabbitModule } from "../common/brokers/rabbit.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course, User])],
+  imports: [TypeOrmModule.forFeature([Course, User]), RabbitModule],
   providers: [CourseService],
   exports: [CourseService],
 })
