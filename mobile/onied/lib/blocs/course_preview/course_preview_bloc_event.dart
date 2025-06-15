bf33793e684ed93d951251e8d6294f4523ac7b55
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2025-06-15 19:49:16 +0300

    Added most of the code for stats in mobile

--- mobile/onied/lib/blocs/course_preview/course_preview_bloc_event.dart
+++ mobile/onied/lib/blocs/course_preview/course_preview_bloc_event.dart
@@ -4,3 +4,17 @@ class LoadCoursePreview extends CoursePreviewBlocEvent {
   final String courseId;
   LoadCoursePreview(this.courseId);
 }
+
+class LikeCurrentCourse extends CoursePreviewBlocEvent {
+  final bool like;
+  LikeCurrentCourse(this.like);
+}
+
+class OpenStats extends CoursePreviewBlocEvent {}
+
+class CloseStats extends CoursePreviewBlocEvent {}
+
+class UpdateStats extends CoursePreviewBlocEvent {
+  final int likes;
+  UpdateStats(this.likes);
+}
--- mobile/onied/lib/pages/course_preview/course_preview_page.dart
+++ mobile/onied/lib/pages/course_preview/course_preview_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:dart_amqp/dart_amqp.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:onied_mobile/app/config.dart';
 import 'package:onied_mobile/app/injection.dart';
 import 'package:onied_mobile/blocs/course_preview/course_preview_bloc.dart';
 import 'package:onied_mobile/blocs/course_preview/course_preview_bloc_event.dart';
@@ -7,6 +11,7 @@ import 'package:onied_mobile/blocs/course_preview/course_preview_bloc_state.dart
 import 'package:onied_mobile/components/button/button.dart';
 import 'package:onied_mobile/components/picture_preview/picture_preview.dart';
 import 'package:onied_mobile/components/search_bar/search_bar.dart';
+import 'package:onied_mobile/models/course_preview_model.dart';
 import 'package:onied_mobile/providers/courses_provider.dart';
 import 'package:onied_mobile/repositories/course_repository.dart';
 import 'components/allow_certificates.dart';
@@ -14,10 +19,125 @@ import 'components/author_block.dart';
 import 'components/course_program.dart';
 import 'package:flutter/material.dart';
 
-class CoursePreviewPage extends StatelessWidget {
+enum OpenedTab { description, stats }
+
+Future<Consumer> _initConsumer() async {
+  final client = Client(
+    settings: ConnectionSettings(
+      host: Config.rabbitMqHost,
+      port: 5672,
+      authProvider: const PlainAuthenticator('user', 'useruser'),
+    ),
+  );
+  final channel = await client.channel();
+
+  // Создаем очередь
+  final queue = await channel.queue('onied-stats-queue', durable: true);
+  // Создаем потребителя, с тегом my_consumer
+  final consumer = await queue.consume(consumerTag: 'mobile');
+  return consumer;
+}
+
+class CoursePreviewPage extends StatefulWidget {
   final String courseId;
 
   const CoursePreviewPage({super.key, required this.courseId});
+
+  @override
+  State<CoursePreviewPage> createState() => _CoursePreviewPageState();
+}
+
+class _CoursePreviewPageState extends State<CoursePreviewPage> {
+  late final StreamSubscription streamSubscription;
+  Consumer? consumer;
+
+  @override
+  initState() {
+    super.initState();
+    _initConsumer().then((cons) {
+      setState(() {
+        consumer = cons;
+        streamSubscription = cons.listen((AmqpMessage amqpMessage) {
+          context.read<CoursePreviewBloc>().add(
+            UpdateStats(amqpMessage.payloadAsJson["likes"]),
+          );
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    streamSubscription.cancel();
+    consumer?.cancel();
+    super.dispose();
+  }
+
+  Widget buildHeader(
+    CoursePreviewModel course,
+    OpenedTab openedTab,
+    CoursePreviewBloc bloc,
+  ) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PreviewPicture(
+              width: 120,
+              height: 180,
+              href: course.pictureHref,
+              isArchived: course.isArchived,
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    course.title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4),
+                  AuthorBlock(
+                    authorName: course.courseAuthor.name,
+                    authorAvatarHref: course.courseAuthor.avatarHref,
+                  ),
+                  SizedBox(height: 4),
+                  Text(course.category.name, style: TextStyle(fontSize: 14)),
+                  course.hasCertificates
+                      ? AllowCertificate()
+                      : SizedBox.shrink(),
+                ],
+              ),
+            ),
+          ],
+        ),
+        SegmentedButton(
+          segments: [
+            ButtonSegment(
+              value: OpenedTab.description,
+              label: Text("Описание"),
+              icon: Icon(Icons.description),
+            ),
+            ButtonSegment(
+              value: OpenedTab.stats,
+              label: Text("Статистика"),
+              icon: Icon(Icons.analytics),
+            ),
+          ],
+          selected: {openedTab},
+          onSelectionChanged: (Set<OpenedTab> newSelection) {
+            if (newSelection.first == OpenedTab.description) {
+              bloc.add(CloseStats());
+            } else {
+              bloc.add(OpenStats());
+            }
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +145,7 @@ class CoursePreviewPage extends StatelessWidget {
       create:
           (context) => CoursePreviewBloc(
             courseRepository: CourseRepository(getIt<CourseProvider>()),
-          )..add(LoadCoursePreview(courseId)),
+          )..add(LoadCoursePreview(widget.courseId)),
       child: Scaffold(
         appBar: const CourseSearchBar(),
         body: BlocBuilder<CoursePreviewBloc, CoursePreviewBlocState>(
@@ -43,58 +163,38 @@ class CoursePreviewPage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          PreviewPicture(
-                            width: 120,
-                            height: 180,
-                            href: course.pictureHref,
-                            isArchived: course.isArchived,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  course.title,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(height: 4),
-                                AuthorBlock(
-                                  authorName: course.courseAuthor.name,
-                                  authorAvatarHref:
-                                      course.courseAuthor.avatarHref,
-                                ),
-                                SizedBox(height: 4),
-                                Text(
-                                  course.category.name,
-                                  style: TextStyle(fontSize: 14),
-                                ),
-                                course.hasCertificates
-                                    ? AllowCertificate()
-                                    : SizedBox.shrink(),
-                              ],
-                            ),
-                          ),
-                        ],
+                      buildHeader(
+                        course,
+                        OpenedTab.description,
+                        context.read<CoursePreviewBloc>(),
                       ),
                       Container(
                         width: double.infinity,
                         padding: EdgeInsets.symmetric(vertical: 16),
-                        child: Button(
-                          text: course.isOwned ? "продолжить" : "купить",
-                          onPressed: () {
-                            if (course.isOwned) {
-                              context.push("/course/${course.id}/learn");
-                            } else {
-                              context.push("/purchase");
-                            }
-                          },
+                        child: Column(
+                          children: [
+                            Button(
+                              text: course.isOwned ? "продолжить" : "купить",
+                              onPressed: () {
+                                if (course.isOwned) {
+                                  context.push("/course/${course.id}/learn");
+                                } else {
+                                  context.push("/purchase");
+                                }
+                              },
+                            ),
+                            Button(
+                              text:
+                                  course.isLiked
+                                      ? "убрать из понравившихся"
+                                      : "в понравившиеся",
+                              onPressed: () {
+                                context.read<CoursePreviewBloc>().add(
+                                  LikeCurrentCourse(!course.isLiked),
+                                );
+                              },
+                            ),
+                          ],
                         ),
                       ),
                       // Description
@@ -108,6 +208,23 @@ class CoursePreviewPage extends StatelessWidget {
                   ),
                 ),
               ),
+              StatsOpenState(:final course, :final likes) =>
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        buildHeader(
+                          course,
+                          OpenedTab.stats,
+                          context.read<CoursePreviewBloc>(),
+                        ),
+                        Text("В понравившихся: $likes"),
+                      ],
+                    ),
+                  ),
+                ),
               _ => const Center(child: Text("Something went wrong.")),
             };
           },
